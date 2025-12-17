e56b121f525d8db609e52a378d8def3ff400c466
JJacKTH <109510740+JJacKTH@users.noreply.github.com>
2025-12-18 00:41:53 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,24 @@
+const params = new URLSearchParams(location.search);
+const id = params.get("id");
+
+if (!id) {
+  location.href = "index.html";
+}
+
+fetch(API_URL + "?action=me&id=" + id)
+  .then(r => r.json())
+  .then(user => {
+    if (user.error) throw "not login";
+
+    document.getElementById("welcome").innerText =
+      "ยินดีต้อนรับ " + user.name;
+
+    if (user.role === "admin") {
+      document.getElementById("adminPanel").classList.remove("hidden");
+      loadMembers();
+    }
+  })
+  .catch(() => {
+    location.href = "index.html";
+  });
 
