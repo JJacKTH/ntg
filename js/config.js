b5b54fb9f4f39994e2b18f92820ddaee42ad09df
JJacKTH <109510740+JJacKTH@users.noreply.github.com>
2025-12-18 01:15:13 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const DISCORD_CLIENT_ID = "1450872825228300419";
 const OAUTH_URL = "https://discord.com/api/oauth2/authorize";
-const APP_SCRIPT_URL = "YOUR_APP_SCRIPT_URL"; 
+const APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw617uqWpeplXHI8Q2YK16aEKZAO6eBMW-9yT_NZ5WKjXxkhHAe1kcjVfwxolujgfrW/exec"; 
 const REDIRECT_URI = APP_SCRIPT_URL;
 
