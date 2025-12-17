13f319ba6a9f167d1149c898461fe884b89292ef
JJacKTH <109510740+JJacKTH@users.noreply.github.com>
2025-12-18 00:45:13 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,5 @@
+const DISCORD_CLIENT_ID = "1450872825228300419";
+const OAUTH_URL = "https://discord.com/api/oauth2/authorize";
+const APP_SCRIPT_URL = "YOUR_APP_SCRIPT_URL"; 
+const REDIRECT_URI = APP_SCRIPT_URL;
 
