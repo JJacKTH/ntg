976928714b361a9096eab78ffad45a6f9d308010
JJacKTH <109510740+JJacKTH@users.noreply.github.com>
2025-12-18 01:58:43 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const DISCORD_CLIENT_ID = "1450872825228300419";
 const OAUTH_URL = "https://discord.com/api/oauth2/authorize";
-const APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw617uqWpeplXHI8Q2YK16aEKZAO6eBMW-9yT_NZ5WKjXxkhHAe1kcjVfwxolujgfrW/exec"; 
+const APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwjnJWHkPdCCI5q9AcAT6LJmnTYZq3c5-rgMyucuge4TwA-WtPKPoVjbR8x6WO7K9Sw/exec"; 
 const REDIRECT_URI = APP_SCRIPT_URL;
 
