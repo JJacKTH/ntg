0abc8e4213d1c17d8b3436a22c246d02be65b986
JJacKTH <109510740+JJacKTH@users.noreply.github.com>
2025-12-18 00:45:25 +0700

    Add OAuth login button functionality

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,11 @@
+document.getElementById("loginBtn").addEventListener("click", () => {
+  const params = new URLSearchParams({
+    client_id: DISCORD_CLIENT_ID,
+    redirect_uri: REDIRECT_URI,
+    response_type: "code",
+    scope: "identify"
+  });
+
+  location.href = `${OAUTH_URL}?${params.toString()}`;
+});
 
